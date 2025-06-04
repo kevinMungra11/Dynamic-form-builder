f67fd0cae413402a5c11a3833cfdcbcce036911f
kevin mungra <kp496719@gmail.com>
2025-06-04 13:01:53 +0000

    Basic server setup

---
+++ backend/server.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import connectDB from "./config/db";
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.listen(PORT, () => {
+  console.log("server is up and running !!");
+});
+
+export default app;
